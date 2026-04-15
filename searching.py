f544cae9308ae93a6a7b432fd0c119647c1b31fe
vilemb285 <283244@vutbr.cz>
2026-04-15 09:42:36 +0200

    ukol2

--- searching.py
+++ searching.py
@@ -3,7 +3,10 @@ import json
 
 
 def read_data(file_name, field):
-    with open("file_name", "r" ) as f:
+    cwd_path = Path.cwd()
+    file_path = cwd_path / file_name
+
+    with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
     if field not in data:
@@ -11,6 +14,23 @@ def read_data(file_name, field):
 
     return data[field]
 
+def linear_search(sequence, found_key):
+    dict = {
+        "positions": [],
+        "count" : 0
+    }
+
+    for i, value in enumerate(sequence):
+        if found_key == value:
+            dict["positions"].append(i)
+            dict["count"] += 1
+    return dict
+
 def main():
-    data = read_data("sequential.json", "unordered?numbers")
+    data = read_data("sequential.json", "unordered_numbers")
+    search = linear_search("sequential.json",2)
     print(data)
+    print(search)
+
+if __name__ == "__main__":
+    main()
